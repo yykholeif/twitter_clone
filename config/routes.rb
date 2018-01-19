f8bab3f6dfde424809be15971b84590d10a6f78d
Youssef Kholeif <yykholeif@gmail.com>
2018-01-19 09:16:52 +0000

    modifying routes to create additional shortcuts

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,14 @@ Rails.application.routes.draw do
 
   root 'static_pages#home'
   
-  get 'static_pages/home'
+  # get 'static_pages/home'
 
-  get 'static_pages/help'
+  # get '/help', to: 'static_pages#help', as: 'helf'
+  
+  get '/help', to: 'static_pages#help'
 
-  get 'static_pages/about'
+  get '/about', to: 'static_pages#about'
 
-  get 'static_pages/contact'
+  get '/contact', to: 'static_pages#contact'
 
 end
