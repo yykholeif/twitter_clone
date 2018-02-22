e7909faf70a1190a661513ba6625c930fd574c09
Youssef Kholeif <yykholeif@gmail.com>
2018-02-22 18:59:32 +0200

    Write test for displaying error messages during signup

---
+++ test/integration/users_signup_test.rb
@@ -0,0 +1,18 @@
+require 'test_helper'
+
+class UsersSignupTest < ActionDispatch::IntegrationTest
+  
+  test "invalid signup information" do
+  	get signup_path
+  	assert_no_difference 'User.count' do
+  		post signup_path, params: { user: { name: "",
+  										   email: "user@invalid",
+  										   password: "foo",
+  										   password_confirmation: "bar"}}
+  end
+  assert_template 'users/new'
+  assert_select '#error_explanation'
+  assert_select '.alert'
+  assert_select 'form[action="/signup"]'
+ end  
+end
