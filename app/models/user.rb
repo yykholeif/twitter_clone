6ed4a6309a1e5ed531aaab1cc41a16cb46b8811e
Youssef Kholeif <yykholeif@gmail.com>
2018-03-22 12:07:06 +0000

    persist user login information using cookies

--- app/models/user.rb
+++ app/models/user.rb
@@ -25,4 +25,8 @@ class User < ApplicationRecord
 		update_attribute(:remember_digest, User.digest(remember_token))
 	end 
 
+	def authenticated?(remember_token)
+		BCrypt::Password.new(remember_digest).is_password?(remember_token)
+	end 
+
 end
